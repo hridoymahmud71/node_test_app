f89275ff5864bcf06ea1c284aa3207460432cc12
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-12-28 12:06:46 +0600

    file uploaded

--- utilities/uploads.js
+++ utilities/uploads.js
@@ -1,8 +1,24 @@
 const multer = require("multer");
+var path = require("path");
 
 
-const UPLOADS_FOLDER = "../uploads/";
-var uploads = multer({dest:UPLOADS_FOLDER});
+const UPLOADS_FOLDER =  path.join(__dirname, './../uploads');
+
+const storage = multer.diskStorage(
+    {
+        destination:(req,file,cb) => {
+            cb(null,UPLOADS_FOLDER);
+        },
+
+        filename:(req,file,cb) => {
+            const  fileExt  = path.extname(file.originalname);
+            const  fileName  = file.originalname.replace(fileExt,"").toLowerCase().split(" ").join("-")+ Date.now();
+            cb(null,`${fileName}${fileExt}`)
+        }
+    }
+)
+
+var uploads = multer({storage:storage});
 
 module.exports = uploads;
 
