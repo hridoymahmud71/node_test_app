8547a578393e05d8eb04e7b12c09fda46095f2c2
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-12-28 11:10:56 +0600

    node modules removed

--- utilities/uploads.js
+++ utilities/uploads.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 
-const UPLOADS_FOLDER = "../uploads";
+const UPLOADS_FOLDER = "../uploads/";
 var uploads = multer({dest:UPLOADS_FOLDER});
 
 module.exports = uploads;
